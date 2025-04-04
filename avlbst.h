206cf5d161d87603a42becbfa90c425dfaef1dd3
Raptor English <rmenglis@usc.edu>
2025-04-04 02:44:06 +0000

    finalizing

--- avlbst.h
+++ avlbst.h
@@ -176,7 +176,7 @@ void AVLTree<Key, Value>::update_heights(AVLNode<Key, Value>* node) {
     node->setBalance(left_height - right_height);
 
     // Update height (1 + max of left and right subtree heights)
-    node->set_height(std::max(left_height, right_height) + 1);
+    node->set_height(std::max(left_height, right_height) + 1);;
 }
 
 
